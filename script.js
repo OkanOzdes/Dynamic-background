18021bbe6f98afbd9d3ee9738ef10a02f8aa4fc4
a17okaoz <a17okaoz@student.his.se>
2020-11-01 21:59:21 +0100

    project done

---
+++ script.js
@@ -0,0 +1,34 @@
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+var thumbnail = document.getElementsByClassName("thumbnail");
+var hero = document.getElementById("hero");
+
+var backgroundImg = new Array(
+    "Radiance_img/bg1.png",
+    "Radiance_img/bg2.png",
+    "Radiance_img/bg3.png",
+    "Radiance_img/bg4.png",
+    "Radiance_img/bg5.png",
+);
+let i = 0;
+next.onclick = function(){
+
+    if(i < 4) {
+        hero.style.backgroundImage = 'url("'+backgroundImg[i+1]+'")';
+        thumbnail[i+1].classList.add("active");
+        thumbnail[i].classList.remove("active");
+        i++;
+    }
+   
+}
+
+prev.onclick = function(){
+
+    if(i > 0) {
+        hero.style.backgroundImage = 'url("'+backgroundImg[i-1]+'")';
+        thumbnail[i-1].classList.add("active");
+        thumbnail[i].classList.remove("active");
+        i--;
+    }
+   
+}
